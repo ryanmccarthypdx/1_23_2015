eeecc5f3a8a0fc1fc8244abf9d2de5acf1d76f81
Ryan <ryanmccarthypdx@gmail.com>
2015-01-23 11:24:56 -0800

    Add initial sinatra files

--- app.rb
+++ app.rb
@@ -7,4 +7,25 @@ require('pg')
 
 DB = PG.connect({:dbname => "to_do"})
 
-get('/')
+get('/') do
+  @list_of_stylists = Stylists.all()
+  erb(:main)
+end
+
+post('/stylist_post') do
+  @name = params.fetch("stylist_name")
+  new_stylist = Stylist.new({:id => nil, :name => @name})
+  new_stylist.save()
+  @list_of_stylist = Stylists.all()
+  erb(:main)
+end
+
+post('/client_post') do
+  @name = params.fetch("client_name")
+  @phone = params.fetch("phone")
+  @stylist_id = params.fetch("stylist_id")
+  new_client = Client.new({:id => nil, :name => @name, :phone => @phone, :stylist_id => @stylist_id})
+  new_client.save
+  @list_of_stylist = Stylists.all()
+  erb(:main)
+end
